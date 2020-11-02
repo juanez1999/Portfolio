8a8420d83f13e2b604510c434aa8867af49bc7bd
juanez1999 <juanez1999@gmail.com>
2020-11-01 22:23:21 -0500

    Modified section projects

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,3 @@
-// var title = document.querySelector('.mainContainer__infoBlockNameTitle');
-// var secondtitle = document.querySelector('.mainContainer__infoBlockName2Title');
-
-// title.addEventListener('mouseenter', function(){
-//     titleAnimate();
-// });
-
-// secondtitle.addEventListener('mouseenter', function(){
-//     secondtitleAnimate();
-// });
-
-// function titleAnimate(){
-//     micron.getEle(".mainContainer__infoBlockNameTitle").interaction("tada").duration(".45").timing("ease-out");
-// }
-
-// function secondtitleAnimate(){
-//     micron.getEle(".mainContainer__infoBlockName2Title").interaction("tada").duration(".45").timing("ease-out");
-// }
-
-// function animate(){
-//     micron.getEle(".mainContainer__infoBlockNameTitle").interaction("tada").duration(".45").timing("ease-out");
-//     micron.getEle(".mainContainer__infoBlockName2Title").interaction("tada").duration(".45").timing("ease-out");
-// }
-
-// animate();
-
 $(document).ready(function() {
     $(".title").lettering();
   });
@@ -39,4 +13,12 @@ $(document).ready(function() {
     {ease: Back.easeOut.config(0.1), opacity: 0, bottom: -80},
     {ease: Back.easeOut.config(0.3), opacity: 1, bottom: 0}, 0.02);
   }
+
+var btn = document.querySelector('.btnMain');
+
+if(btn != undefined){
+    btn.addEventListener('click',function(){
+      window.location.href = '/Portfolio/portfolio.html';
+    });
+}
 
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -5,21 +5,23 @@ var number = document.querySelector('.mainContainer__projectStateNumber');
 
 const carrousel = document.querySelector('.carrousel');
 const carrouselStripe = document.querySelector('.carrousel__stripe');
+const project = document.querySelector('.mainContainer__projectItem');
 let current = 0;
 
 function handleNextSlide () {
-    console.log(carrouselStripe.children.length);
+    // console.log(carrouselStripe.children.length);
 
     current++;
     if(current >= carrouselStripe.children.length) {
         current = 0;
     }
     number.innerHTML = current+1;
-    const height = carrousel.clientHeight;
+    const height = project.clientHeight;
     carrouselStripe.style.transform = 'translate(0px, -' + (height * current) + 'px)';
-    console.log(current);
+    console.log(carrousel.clientHeight);
 
 }
+console.log(window.innerHeight);
 
 arrow.addEventListener('click', handleNextSlide);
 
